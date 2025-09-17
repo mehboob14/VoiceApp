e807d1cc72cac2d21f24e7e5d0c53acc91b9316a
Mehboob Arshad <130151311+mehboob14@users.noreply.github.com>
2025-09-17 19:34:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -284,6 +284,7 @@ async def get():
     </html>
     """
 
+import os
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
